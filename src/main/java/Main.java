0a6e48e682bb20d532fdba7640afa0bb5d0ed4d7
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-02-16 19:26:54 +0900

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("gs");
+        String s = "";
+        for (int i = 0; i < 100; i++) {
+            s += "fgss";
+        }
+        System.out.println(s);
+    }
+
+}
